e61dd45325bd0dd1508659090b175f542b8168c0
Eliza Vorobiev <vorobieveliza@gmail.com>
2017-04-27 16:22:25 +0200

    fixed freq_filter

--- freq_filter_image.m
+++ freq_filter_image.m
@@ -6,7 +6,7 @@ function [ filt_img ] = freq_filter_image( img, D0 )
 
 % Create the fourier transform of the image
 imgDim = size(img);
-fimg_Original = fft2(double(img),imgDim(1),imgDim(2));
+fimg_Original = fft2(double(img));
 fimg = fftshift(fimg_Original);
 %fimg_shifted = abs(fftshift(fimg));
 % Create the mask using the formula
@@ -14,22 +14,20 @@ H = zeros(imgDim(1),imgDim(2));
 % Go through each pixel and get H value
 % Note: should we be using fimg to create this H?\
 % D is the normalized distance to centre
-% meanFreq = mean(fimg(:));
-% center = find(fimg == meanFreq);
-% centreX=center(1)
-% centreY=center(2)
-centreX=imgDim(1)/2;
-centreY=imgDim(2)/2;
+ meanValue = mean(img(:));
+[centreX, centreY] = find(fimg == meanValue*(320*440));
 maxDistance = sqrt(centreX^2 + centreY^2);
+% this will always be imgY/2 + 1 for even 
 for x = 1:imgDim(1)
     for y = 1:imgDim(2)
-        H(x,y) = 1/(sqrt(1 + ((sqrt((x-centreX)^2 + (y-centreY)^2)/maxDistance)^2)/0.1)^(2*2));
+        H(x,y) = 1/(sqrt(1 + ((sqrt((x-centreX)^2 + (y-centreY)^2)./maxDistance)^2)/0.1)^(2*2));
     end 
 end 
 
 % mulitpy img and mask in frequency domain
 filt_img_freq = fimg .* H;
-filt_img = ifftshift(ifft(filt_img_freq));
-% imshow(filt_img,[]);
+filt_img = real(ifft2(ifftshift(filt_img_freq)));
+figure
+imshow(filt_img,[]);
 end
 
