7ca66369b49dc8689e70827a66ebc667ec1cca08
Eliza Vorobiev <vorobieveliza@gmail.com>
2017-04-27 16:36:35 +0200

    remove extra comments

--- segment_image_otsu.m
+++ segment_image_otsu.m
@@ -17,35 +17,25 @@ sizeim = size(image);
 [hist, values] = createHistogram(image);
 probhist = hist ./ sum(hist);
 
-% bcv = zeros(1, length(hist));
 bcv2 = zeros(1, length(hist));
-% w1 = probhist(1);
 
-for t = 1:length(hist) %currently including value at index t in threshold. idk if this is right.
+for t = 1:length(hist) 
     weightback = sum(probhist(1:t));    
     weightobj = 1-weightback;
     
     meanback = (sum(probhist(1:t).* values(1:t))) ./ weightback; %this shoud img value not hist
     meanobj = sum(probhist(t+1:length(hist)) .* values(t+1:length(hist))) ./ weightobj;
     
-    %meanG = weightback*meanback + weightobj*meanobj; %almost the same as meanobj
-    %meant = sum(probhist(1:t).* hist(1:t)); %same as meanback 
-    
-    %sigmab2 = ((meanG*weightback - meant)^2)/(weightback * weightobj); %i'm not sure if this is the correct formula. the internet says the second one.
     sigmab3 = weightback * weightobj * ((meanback - meanobj)^2);
     
-    %bcv(t) = sigmab2;
     bcv2(t) = sigmab3;
     
 end
 
-%level = find(bcv == max(bcv)) - abs(min(image(:))); %WRONG
 level = find(bcv2 == max(bcv2)) + min(image(:))-1;
-%find(bcv2 == max(bcv2))
-%you can do the segmentation mask in one line (check notes)
+
 segmented_mask = zeros(sizeim);
 segmented_mask(image > level) = 1;
 stats=0;
-%stats = 
 
 end
