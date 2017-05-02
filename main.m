9585ddc7d5086d6275258e014831b0bb8d49b363
Eliza Vorobiev <vorobieveliza@gmail.com>
2017-05-02 16:22:01 +0200

    removed comments

--- main.m
+++ main.m
@@ -2,9 +2,5 @@ close all
 img_id = fopen('CT_Slice_noise.raw','r','l');
 img = fread(img_id, [320,440], 'int16', 0, 'l' );
 d0 = 0.3;
-%filt_img = freq_filter_image(img,d0);
+filt_img = freq_filter_image(img,d0);
 [ segmented_mask, level, stats] = segment_image_otsu( img, 2, [0.74 0.74], 1.5);
-
-% [x, y] = size(img)
-% lengthOfHisto= x*y;
-% levelTest = graythresh(greyImg).* lengthOfHisto
